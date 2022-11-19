5f65f71bd8f91ec1316007b24863d19ca46bb4c4
AnneKin <annekhd98@gmail.com>
2022-11-19 16:22:31 -0300

    joi schema

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
+import joi from 'joi'
 
 //config:
 const app = express()
@@ -23,9 +24,18 @@ let db = mongoClient.db("wallet")
 //globals
 const usersColl = db.collection("users")
 
+//schemas
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email({ minDomainSegments: 2 }).lowercase().required(),
+    password: joi.required,
+    confirmp: joi.required()
+})
+
 
 app.post('/sign-up', async (req, res) => {
     const {name, email, password, confirmp} = req.body
+
 
 
     if(password != confirmp){
@@ -33,11 +43,18 @@ app.post('/sign-up', async (req, res) => {
         return
     }
 
-
     const user = {
         name: name,
         email: email,
         password: password
+    }
+
+    const validation = userSchema.validate(user, {abortEarly: false})
+
+    if(validation.error){
+        const errors = validation.error.details.map( (detail)=> detail.message)
+        res.status(422).send("Preencha os campos corretamente")
+        return
     }
 
     try{
