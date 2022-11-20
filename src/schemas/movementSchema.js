9fef2278ab0463b299ea83b24e3f3543c3afafef
AnneKin <annekhd98@gmail.com>
2022-11-20 17:26:48 -0300

    dayjs entry and exit

--- src/schemas/movementSchema.js
+++ src/schemas/movementSchema.js
@@ -3,6 +3,8 @@ import joi from 'joi';
  const movementSchema = joi.object({
     valor: joi.number().required(),
     descricao: joi.required(),
+    data:joi.required(),
+    type: joi.required()
   });
   
 
