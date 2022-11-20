4d40d1154ea9f4035c179d9a81c94dd22d66108a
AnneKin <annekhd98@gmail.com>
2022-11-20 12:31:03 -0300

    authController

---
+++ src/controllers/authController.js
@@ -0,0 +1,78 @@
+import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+import {db, usersColl, sessionsColl,signupSchema, signinSchema } from "../index.js"
+
+
+export async function signUp (req, res){
+    const { name, email, password, confirmp } = req.body;
+  
+    const alreadyExists = await usersColl.findOne({ email: email });
+  
+    if (alreadyExists) {
+      res.status(400).send("Email já cadastrado.");
+      return;
+    }
+  
+    if (password != confirmp) {
+      res.status(400).send("As senhas não conferem!");
+      return;
+    }
+  
+    const passwordHash = bcrypt.hashSync(password, 10);
+  
+    const user = {
+      name: name,
+      email: email,
+      password: passwordHash,
+      confirmp: passwordHash,
+    };
+  
+    const validation = signupSchema.validate(user, { abortEarly: false });
+  
+    if (validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
+      res.status(422).send("Preencha os campos corretamente");
+      console.log(errors);
+      return;
+    }
+  
+    try {
+      await usersColl.insertOne({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+      res.status(201).send("Created");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+export async function signIn (req, res) {
+    const { email, password } = req.body;
+    const token = uuidV4();
+  
+    const validation = signinSchema.validate(req.body, { abortEarly: false });
+  
+    if (validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
+      res.status(422).send("Preencha os campos corretamente");
+      console.log(errors);
+      return;
+    }
+  
+    const user = await usersColl.findOne({ email });
+    const userId = user._id
+  
+    if (user && bcrypt.compareSync(password, user.password)) {
+      await sessionsColl.insertOne({
+        token,
+        userId: user._id,
+      });
+  
+      res.send({ user, token });
+    } else {
+      res.status(401).send("Usuário ou senha incorretos.");
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { MongoClient } from "mongodb";
 import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuidV4 } from "uuid";
+import { signIn, signUp } from "./controllers/authController.js";
 
 //config:
 const app = express();
@@ -20,104 +21,35 @@ try {
   console.log(err);
 }
 
-let db = mongoClient.db("wallet");
+export const db = mongoClient.db("wallet");
 
 //globals
-const usersColl = db.collection("users");
-const sessionsColl = db.collection("sessions");
-const movementsColl = db.collection("movements");
+export const usersColl = db.collection("users");
+export const sessionsColl = db.collection("sessions");
+export const movementsColl = db.collection("movements");
 
 //schemas
-const signupSchema = joi.object({
+ export const signupSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email({ minDomainSegments: 2 }).lowercase().required(),
   password: joi.required(),
   confirmp: joi.required(),
 });
 
-const signinSchema = joi.object({
+export const signinSchema = joi.object({
   email: joi.string().email({ minDomainSegments: 2 }).lowercase().required(),
   password: joi.required(),
 });
 
-const movementSchema = joi.object({
+export const movementSchema = joi.object({
   valor: joi.number().required(),
   descricao: joi.required(),
 });
 
 //routes
-app.post("/sign-up", async (req, res) => {
-  const { name, email, password, confirmp } = req.body;
+app.post("/sign-up", signUp);
 
-  const alreadyExists = await usersColl.findOne({ email: email });
-
-  if (alreadyExists) {
-    res.status(400).send("Email já cadastrado.");
-    return;
-  }
-
-  if (password != confirmp) {
-    res.status(400).send("As senhas não conferem!");
-    return;
-  }
-
-  const passwordHash = bcrypt.hashSync(password, 10);
-
-  const user = {
-    name: name,
-    email: email,
-    password: passwordHash,
-    confirmp: passwordHash,
-  };
-
-  const validation = signupSchema.validate(user, { abortEarly: false });
-
-  if (validation.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
-    res.status(422).send("Preencha os campos corretamente");
-    console.log(errors);
-    return;
-  }
-
-  try {
-    await usersColl.insertOne({
-      name: user.name,
-      email: user.email,
-      password: user.password,
-    });
-    res.status(201).send("Created");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/", async (req, res) => {
-  const { email, password } = req.body;
-  const token = uuidV4();
-
-  const validation = signinSchema.validate(req.body, { abortEarly: false });
-
-  if (validation.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
-    res.status(422).send("Preencha os campos corretamente");
-    console.log(errors);
-    return;
-  }
-
-  const user = await usersColl.findOne({ email });
-  const userId = user._id
-
-  if (user && bcrypt.compareSync(password, user.password)) {
-    await sessionsColl.insertOne({
-      token,
-      userId: user._id,
-    });
-
-    res.send({ user, token });
-  } else {
-    res.status(401).send("Usuário ou senha incorretos.");
-  }
-});
+app.post("/", signIn);
 
 app.post("/new-entry", async (req, res) => {
   const validation = movementSchema.validate(req.body, { abortEarly: false });
