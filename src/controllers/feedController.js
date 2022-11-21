d45dc34c3990871fbbf47cbb6c1ffaa7df085d73
AnneKin <annekhd98@gmail.com>
2022-11-21 13:06:05 -0300

    schema test

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -1,20 +1,32 @@
-import { sessionsColl, movementsColl } from "../index.js"
+import { sessionsColl, movementsColl, usersColl } from "../index.js"
 
 export async function getFeed (req, res) {
     const {authorization} = req.headers
-    const token = authorization?.replace("Bearer", "")
+    const token = authorization?.replace("Bearer ", "")
 
     if(!token){
         res.sendStatus(500)
         return
     }  
 
-    const session = await sessionsColl.find({token})
-
+    const session = await sessionsColl.findOne({token})
+    console.log(session)
     if(!session){
-        res.status(401).send(token)
+        res.sendStatus(401)
         return
     }
+
+    const userId = session.userId
+    const user = await usersColl.findOne({_id: userId})
+   
+    
+    if(!user){
+        res.sendStatus(404)
+        return
+    }
+
+    const userEmail = user.email
+
 
     let entries = await movementsColl.find({"type": "entry"}).toArray()
     let exits = await movementsColl.find({"type": "exit"}).toArray()
@@ -30,7 +42,7 @@ export async function getFeed (req, res) {
 
     
 
-    const movesArray = await movementsColl.find().toArray()
+    const movesArray = await movementsColl.find({email: userEmail}).toArray()
     if(movesArray){
         res.send([movesArray, saldo])
     }else{
